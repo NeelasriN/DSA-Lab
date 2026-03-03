b7d37256ddcd690aaf92a35848e168f71fdd5ff0
Neelasri.N <112534120@kanchiuniv.ac.in>
2026-03-02 21:07:46 -0800

    Fix search element program input prompts

--- search an element.c
+++ search an element.c
@@ -1,23 +1,24 @@
 #include<stdio.h>
 int main(){
 int n,i,search,found=0;
-printf("enter number of element in the array:");
+printf("Enter number of elements in the array:");
 scanf("%d",&n);
 int arr[n];
-printf("enter element of the array:\n");
+printf("Enter elements of the array:\n");
 for(i=0;i<n;i++){
 	scanf("%d",& arr[i]);
 }
-printf("enter the element of search:");
-for(i+0;i<n;i++){
-	if(arr[i]== search){
+printf("Enter element to search:");
+for(i=0;i<n;i++){
+	if(arr[i]==search){
 		found=1;
-		printf("element %d found at index %d\n ",search,i);
+		printf("Element %d found at index %d\n ",search,i);
 		break;
 	}
 }
 if(!found){
-	printf("element %d not found in the array \n",search);
+	printf("Element %d not found in the array \n",search);
 }
 return 0;
 }
+
